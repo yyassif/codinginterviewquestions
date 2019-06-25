5fca389b57c2c6f5b9e5035ba5d352215a4775ed
Myungho Sim <danielsim22c@gmail.com>
2019-06-25 14:45:30 -0400

    Update matchingeverythingbutanewline.py

--- regex/matchingeverythingbutanewline.py
+++ regex/matchingeverythingbutanewline.py
@@ -1,6 +1,11 @@
 #Myungho Sim
 #match string with this pattern "abc.def.abc.def"
 #matching everything but a new line from hackerrank
+#reference https://www.oreilly.com/library/view/oracle-regular-expressions/0596006012/re13.html
+#regex anchors http://www.rexegg.com/regex-anchors.html
+#{3} matches "." three times.
+#"." is all the characters except newline
+#^ checks for location of certain patterns
 
 regex_pattern = r"^.{3}\..{3}\..{3}\..{3}$"
 
