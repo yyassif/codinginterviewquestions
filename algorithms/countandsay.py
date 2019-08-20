c77fd2989caac97b050659efd9a23da4077b9fde
Myungho Sim <danielsim22c@gmail.com>
2019-08-19 22:37:41 -0400

    Update countandsay.py

--- algorithms/countandsay.py
+++ algorithms/countandsay.py
@@ -1,29 +1,31 @@
 #Myungho Sim
-#count and say problem from leetcode - work in progress
-def countAndSay(n: int) -> str:
-  ret="1"
-  if n==1:
-      return ret
-  for i in range(n-1):
-      digit=""
-      string = ret
-      size = len(string)
-      cnt=0
-      prev=""
-      for j in range(size):
-        ret =
-          digit = string[j]
-          if(prev==digit):
-              cnt+=1
-              if (j==size-1):
-                  ret=str(cnt)+prev +
-          elif (prev!=digit): #change in digit or reached the end
-              if (j==size-1):
-                  ret+=str(cnt)+digit
-                  print("#",ret)
-              else:
-                  ret+=str(cnt)+prev
-                  cnt=1
-          prev = string[j]
-  return ret
-print(countAndSay(2))
+#count and say problem @leetcode
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        a="1"
+        newstr=""
+        if n==1:
+            return "1"
+        for j in range(n-1):
+            newstr = ""
+            size = len(a)
+            digit=""
+            if size==1:
+                newstr = "1"+a
+            else:
+                cnt=1
+                for i in range(size-1):
+                    if a[i]==a[i+1]:
+                        cnt+=1
+                    elif a[i]!=a[i+1]:
+                        digit = a[i]
+                        newstr += str(cnt)+digit
+                        cnt=1
+                newstr += str(cnt) + a[size-1]
+            a = newstr
+            print(a)
+        return newstr
