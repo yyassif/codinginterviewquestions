0f20290e8bfa9da1b49b4b3f67a346378fa44cde
Myungho Sim <danielsim22c@gmail.com>
2019-08-21 13:55:41 -0400

    Update rangesumbst.py

--- algorithms/rangesumbst.py
+++ algorithms/rangesumbst.py
@@ -1,5 +1,6 @@
 #Myungho Sim
 #range sum bst from leetcode
+#less efficient impl using preorder visit to every node. beats 5% of submissions
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -20,6 +21,22 @@ class Solution:
             if r<L and r>R:
                 r=0
             return v+l+r
+        else:
+            return 0
+            
+#BST search algo, avoids visitng nodes outside of the range. beats 94% of submissions
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if root:
+            v = root.val
+            if v<L:
+                return self.rangeSumBST(root.right, L,R)
+            elif v>R:
+                return self.rangeSumBST(root.left, L,R)
+            else:
+                l = self.rangeSumBST(root.left, L,R)
+                r = self.rangeSumBST(root.right, L,R)
+                return v+l+r
         else:
             return 0
             
