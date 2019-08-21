45d6f4dcb023748b2c0062cc73bdce46d57caf37
Myungho Sim <danielsim22c@gmail.com>
2019-08-20 22:15:06 -0400

    Create reversealinkedlist.py

---
+++ algorithms/reversealinkedlist.py
@@ -0,0 +1,18 @@
+#Myungho Sim
+#reverse a linked list - 99% better than all submissions on leetcode
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        curr = head
+        prev = None
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
