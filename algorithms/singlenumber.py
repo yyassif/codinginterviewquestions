d7ece74355cea4dbb774f1482d3fe4693eee6b20
Myungho Sim <danielsim22c@gmail.com>
2019-08-21 13:17:40 -0400

    Update singlenumber.py

--- algorithms/singlenumber.py
+++ algorithms/singlenumber.py
@@ -1,6 +1,8 @@
 #Myungho Sim
 #single number problem @ leetcode  - using hashmap
 #25% better than other submissions. needs improvement
+#this is better than brute force method which loop over the number for each digit. In that case running time is O(n^2)
+#using hash map, the running time is O(n)
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         map = {}
