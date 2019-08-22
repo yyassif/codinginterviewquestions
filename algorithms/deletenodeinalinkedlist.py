dd9bf0b8eb7b98c0bd8837ab04cc5ddd337f9303
Myungho Sim <danielsim22c@gmail.com>
2019-08-21 23:07:19 -0400

    Update deletenodeinalinkedlist.py

--- algorithms/deletenodeinalinkedlist.py
+++ algorithms/deletenodeinalinkedlist.py
@@ -5,15 +5,20 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+#better solution based on an idea taken from leetcode discussion. replace value with the next node and connect to next next node
 class Solution:
     def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
-        while node.next:
-            node.val = node.next.val
-            prev = node
-            node = node.next
-        prev.next = None
+        node.val = node.next.val
+        node.next = node.next.next
+#solution 2 - slower. shift entire rest of the linked list
+# class Solution:
+#     def deleteNode(self, node):
+#         """
+#         :type node: ListNode
+#         :rtype: void Do not return anything, modify node in-place instead.
+#         """
+#         while node.next:
+#             node.val = node.next.val
+#             prev = node
+#             node = node.next
+#         prev.next = None
