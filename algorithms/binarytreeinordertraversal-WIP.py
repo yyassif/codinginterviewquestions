2d1fd039e4649d23a9f252f900edcd99da06e422
Myungho Sim <danielsim22c@gmail.com>
2019-08-22 18:58:52 -0400

    Update binarytreeinordertraversal-WIP.py

--- algorithms/binarytreeinordertraversal-WIP.py
+++ algorithms/binarytreeinordertraversal-WIP.py
@@ -1,23 +1,23 @@
+#Myungho Sim
+#Binary Tree Inorder Traversal
+#faster than 92% of submissions on leetcode
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution:
-    arr=[]
-    def __init__(self):
-        arr=[]
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        arr=[]
-        if root:
-            if root.left:
-                self.inorderTraversal(root.left)
-            self.arr.append(root.val)
-            if root.right:
-                self.inorderTraversal(root.right)
-        else:
-            return None
-        return self.arr
-            
+        arr = []
+        def inorder(root):
+            if root:
+                if root.left:
+                    inorder(root.left)
+                arr.append(root.val)
+                if root.right:
+                    inorder(root.right)
+            else:
+                return None
+        inorder(root)
+        return arr
