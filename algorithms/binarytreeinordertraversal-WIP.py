908af6588b05771a70dbe10f2ecd678a10be721e
Myungho Sim <danielsim22c@gmail.com>
2019-08-22 21:59:26 -0400

    Update binarytreeinordertraversal-WIP.py

--- algorithms/binarytreeinordertraversal-WIP.py
+++ algorithms/binarytreeinordertraversal-WIP.py
@@ -1,23 +1,38 @@
 #Myungho Sim
-#Binary Tree Inorder Traversal
-#faster than 92% of submissions on leetcode
+#binary search inorder traversal. iterative and recursive methods.
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+#iterative solution
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        arr = []
-        def inorder(root):
+        ret = []
+        stack = []
+        while stack or root:
             if root:
-                if root.left:
-                    inorder(root.left)
-                arr.append(root.val)
-                if root.right:
-                    inorder(root.right)
+                stack.append(root)
+                root = root.left
             else:
-                return None
-        inorder(root)
-        return arr
+                node = stack.pop()
+                ret.append(node.val)
+                root = node.right
+        return ret
+#recursive solution        
+# class Solution:
+#     def inorderTraversal(self, root: TreeNode) -> List[int]:
+#         arr = []
+#         def inorder(root):
+#             if root:
+#                 if root.left:
+#                     inorder(root.left)
+#                 arr.append(root.val)
+#                 if root.right:
+#                     inorder(root.right)
+#             else:
+#                 return None
+#         inorder(root)
+#         return arr
