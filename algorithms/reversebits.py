40ace6a0eb839454c3bfbed43a96969cac251355
Myungho Sim <danielsim22c@gmail.com>
2019-09-05 00:56:07 -0400

    Create reversebits.py

---
+++ algorithms/reversebits.py
@@ -0,0 +1,10 @@
+#reverse bits
+#referenced from https://leetcode.com/problems/reverse-bits/discuss/54792/one-line-python-solution-with-brief-explanation
+# '{0:032b}'.format(n)
+# The first '0' - format the 0th(the first) argument; 
+# the second '0'- for padding
+# '32' - total length of the output
+# 'b' - binary.
+class Solution:
+  def reverseBits(self, n):
+    return int('{0:032b}'.format(n)[::-1], 2)
