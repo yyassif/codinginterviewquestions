59cc0b68646dd54f38390314608b38872e072a9a
Myungho Sim <danielsim22c@gmail.com>
2019-09-09 16:39:37 -0400

    Update swapsalary.sql

--- databases/swapsalary.sql
+++ databases/swapsalary.sql
@@ -1,8 +1,11 @@
 # Myungho Sim
 #swap sex variable's value from m to f and f to m
-#solution using case when then
+#solution using case when then - from leetcode discussion
 update salary 
 set sex = case
 when sex='f' then 'm'
 when sex='m' then 'f'
 end
+
+#solution using ternary condtion using if - from leetcode discussion
+update salary set sex=if(sex='f','m','f')
