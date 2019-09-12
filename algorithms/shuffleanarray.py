4c379ef964ec9877f96f0ca12b11ef91ddfad608
Myungho Sim <danielsim22c@gmail.com>
2019-09-12 18:47:16 -0400

    Create shuffleanarray.py

---
+++ algorithms/shuffleanarray.py
@@ -0,0 +1,25 @@
+#Myungho Sim
+import random
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.arr = nums.copy()
+        
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.arr
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        ret = self.arr.copy()
+        random.shuffle(ret)
+        return ret
+        
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
