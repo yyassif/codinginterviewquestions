d1fe1b9652f739b5b63c2426be0c84d3660f2002
Myungho Sim <danielsim22c@gmail.com>
2020-02-17 17:47:56 -0500

    Update 393.utf8_validation.py

--- algorithms/393.utf8_validation.py
+++ algorithms/393.utf8_validation.py
@@ -3,11 +3,11 @@
 
 #requirements
 For 1-byte character, the first bit is a 0, followed by its unicode code.
-For n-bytes character, the first n-bits are all one's, the n+1 bit is 0, **followed by n-1 bytes**(recursive) with most significant 2 bits being 10.
+For n-bytes character, the first n-bits are all one's, the n+1 bit is 0, **followed by n-1 bytes** with most significant 2 bits being 10.
 e.g  4 bytes char = [a,b,c,d]
 the first n-bits of the first number are all one's for the first n bits. For the same num, it is followed by 0 at n+1 bit. 
-the following numbers represent n-1 bytes. 3 byte number has [a,b,c]. each number's most significant 2 bits- last digit is 10. 
-* the relationships are recursive. Nth byte number describes what's immediately behind. 
+the following numbers represent n-1 bytes. 3 byte number has [a,b,c]. each number's most significant 2 bits- leftmost digits are 10. 
+*  Nth byte number describes what's immediately behind. 
 
 #Example 1:
 data = [197, 130, 1], which represents the octet sequence: 11000101 10000010 00000001.
