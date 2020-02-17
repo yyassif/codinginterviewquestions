72e43dd4e2c72988ef73fdb02444059ae762a49a
Myungho Sim <danielsim22c@gmail.com>
2020-02-17 15:59:54 -0500

    Create 393.utf8_validation.py

---
+++ algorithms/393.utf8_validation.py
@@ -0,0 +1,7 @@
+# 393.utf8_validation.py
+#sol: https://leetcode.com/articles/utf-8-validation/
+#Example 1:
+data = [197, 130, 1], which represents the octet sequence: 11000101 10000010 00000001.
+It is a valid utf-8 encoding for a 2-bytes character followed by a 1-byte character.
+2 bytes char: 11000101
+followed by 1bytes char :  10000010
