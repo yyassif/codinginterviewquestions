a8b75f92ced97e32fd2212484e8caf4426ca42ac
Myungho Sim <danielsim22c@gmail.com>
2020-02-27 13:49:17 -0500

    Create 41.firstmissingpositive.py

---
+++ algorithms/41.firstmissingpositive.py
@@ -0,0 +1,49 @@
+#41.firstmissingpositive.py
+#reference https://leetcode.com/articles/first-missing-positive/
+class Solution:
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)  #size of nums array
+        
+        # Base case.
+        if 1 not in nums:
+            return 1
+        
+        # nums = [1]
+        if n == 1:   # size 1 array must have 1 since negative and num>size will be replaced
+            return 2
+        
+        # Replace negative numbers, zeros,
+        # and numbers larger than n by 1s.
+        # After this convertion nums will contain 
+        # only positive numbers.
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = 1
+        
+        # Use index as a hash key and number sign as a presence detector.
+        # For example, if nums[1] is negative that means that number `1`
+        # is present in the array. 
+        # If nums[2] is positive - number 2 is missing.
+        for i in range(n): 
+            a = abs(nums[i])
+            # If you meet number a in the array - change the sign of a-th element.
+            # Be careful with duplicates : do it only once.
+            if a == n:     #encode the number n at index 0 since there are no zeroes. 
+                nums[0] = - abs(nums[0])
+            else:      #if number is not n then encode the number with a negative sign at index==number in nums array
+                nums[a] = - abs(nums[a])
+            
+        # Now the index of the first positive number 
+        # is equal to first missing positive.
+        for i in range(1, n):
+            if nums[i] > 0:
+                return i
+        
+        if nums[0] > 0:
+            return n
+            
+        return n + 1     #no numbers are missing. return the next num in sequence.
