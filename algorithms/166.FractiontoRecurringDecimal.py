7daa6396808ae37134110061bf4953af9be4a7e4
Myungho Sim <danielsim22c@gmail.com>
2020-02-27 21:20:13 -0500

    Create 166.FractiontoRecurringDecimal.py

---
+++ algorithms/166.FractiontoRecurringDecimal.py
@@ -0,0 +1,27 @@
+#166. Fraction to Recurring Decimal
+#sol adapted from leetcode
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        if numerator==0:
+            return "0"
+        frac=""
+        if numerator<0 ^ denominator<0:
+            frac+="-"
+        dividend = abs(numerator)
+        divisor = abs(denominator)
+        frac+=str(dividend//divisor)
+        remainder= numerator%denominator
+        if remainder==0:
+            return frac
+        frac+="."
+        map={}
+        while(remainder!=0):
+            if remainder in map:
+                idx=map[remainder]
+                frac = frac[:idx]+"("+frac[idx:]+")"
+                return frac
+            map[remainder] = len(frac)
+            remainder*=10
+            frac+= str(remainder//divisor)
+            remainder%=divisor    # e.g. 670%333 = 4
+        return frac
