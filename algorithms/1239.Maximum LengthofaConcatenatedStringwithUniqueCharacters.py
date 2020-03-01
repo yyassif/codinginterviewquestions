6e4def8623ff2740eb21f9e71555a6dea7cee7a7
Myungho Sim <danielsim22c@gmail.com>
2020-03-01 18:28:31 -0500

    Create  1239.Maximum LengthofaConcatenatedStringwithUniqueCharacters.py

---
+++ algorithms/1239.Maximum LengthofaConcatenatedStringwithUniqueCharacters.py
@@ -0,0 +1,2 @@
+#1239. Maximum Length of a Concatenated String with Unique Characters
+#sol adapted from https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/discuss/513327/Simple-Backtracking-solution
