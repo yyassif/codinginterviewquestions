4fad3d65d6b511f5d2391144983cb57f7f488f8b
Myungho Sim <danielsim22c@gmail.com>
2020-03-02 00:43:39 -0500

    Create printinorder.py

---
+++ concurrency/printinorder.py
@@ -0,0 +1,3 @@
+# 1114. Print in Order
+# sol from https://leetcode.com/problems/print-in-order/discuss/335939/5-Python-threading-solutions-(Barrier-Lock-Event-Semaphore-Condition)-with-explanation
+# 5 different ways of concurrency in python
