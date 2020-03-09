2306acd2259e5c5be8495954f0c60a1379e2eaa4
Myungho Sim <danielsim22c@gmail.com>
2020-03-09 19:31:18 -0400

    Update 535.encodeanddecodetinyurl.py

--- algorithms/535.encodeanddecodetinyurl.py
+++ algorithms/535.encodeanddecodetinyurl.py
@@ -40,3 +40,48 @@ class Codec:
         if shortUrl in self.map:
             long_url = self.map[shortUrl]
         return long_url
+
+# random method
+import random
+class Codec:
+    def __init__(self):
+        self.map={}
+        self.counter=0
+        self.strSet=""
+        for i in range(0, 10):
+            self.strSet+=str(i)
+        for i in range(65, 65+26):
+            self.strSet+=str(chr(i))
+        for i in range(97, 97+26):
+            self.strSet+=str(chr(i))
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        def getKey():
+            key =""
+            while True:
+                for i in range(7):
+                    rand_num = random.randint(0, 61)   
+                    key+=self.strSet[rand_num]
+                if key in self.map:
+                    continue
+                else:
+                    break
+                print(key)
+            self.map[key] = longUrl
+            return key
+        key=getKey()
+        return "http://tinyurl.com/"+key
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        shortUrl=shortUrl.replace("http://tinyurl.com/", "")
+        if shortUrl in self.map:
+            decoded = self.map[shortUrl]
+        return decoded
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
