1e5f0ccbcb636c7c18ca0b10b66a8b19fcfb01b8
Myungho Sim <danielsim22c@gmail.com>
2020-03-08 21:45:59 -0400

    Update mergetwolists.py

--- algorithms/mergetwolists.py
+++ algorithms/mergetwolists.py
@@ -6,31 +6,18 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l1 is None and l2 is not None:
-            return l2
-        elif l1 is not None and l2 is None:
-            return l1
-        elif l1 is None and l2 is None:
-            return None
-        head = None
-        if l1.val < l2.val:
-            head = l1
-        else:
-            head = l2
-        curr= ListNode(None)
-        while l1 is not None and l2 is not None:
-            if l1.val < l2.val:
-                curr.next = l1
-                curr = l1
-                l1 = l1.next
+        head=ListNode(-1)
+        prev = head
+        while l1 and l2:
+            if l1.val<=l2.val:
+                prev.next = l1
+                l1=l1.next
             else:
-                curr.next =l2
-                curr = l2
-                l2 = l2.next
-        if l1 is None and l2 is not None:
-            curr.next = l2
-        elif l1 is not None and l2 is None:
-            curr.next = l1
-        return head
+                prev.next = l2
+                l2= l2.next
+            prev= prev.next
+        prev.next= l1 or l2
+        return head.next
