de2cb3faba9bdd344d3a551f37c37b3aedfcd9f3
Myungho Sim <danielsim22c@gmail.com>
2020-03-09 17:15:18 -0400

    Update 190.reversebits.py

--- algorithms/190.reversebits.py
+++ algorithms/190.reversebits.py
@@ -8,3 +8,18 @@
 class Solution:
   def reverseBits(self, n):
     return int('{0:032b}'.format(n)[::-1], 2)
+
+#sol 2 https://leetcode.com/problems/reverse-bits/discuss/531594/Python-3-lines-solution
+n_str = bin(n)[2:]
+n_str = n_str.zfill(32)
+return int(n_str[::-1],2)
+
+#bit manipulation method https://leetcode.com/problems/reverse-bits/discuss/271179/Python-Solution-Bit-Manipulation
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        s=""
+        for i in range(32):
+            s=s+str(n>>i & 1)
+        return int(s,2)
