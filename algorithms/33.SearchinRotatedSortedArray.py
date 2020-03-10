a8cccfd12271d01ea959306891392d59362cf508
Myungho Sim <danielsim22c@gmail.com>
2020-03-09 22:59:46 -0400

    Create 33.SearchinRotatedSortedArray.py

---
+++ algorithms/33.SearchinRotatedSortedArray.py
@@ -0,0 +1,22 @@
+# 33. Search in Rotated Sorted Array
+#sol from https://leetcode.com/articles/search-in-rotated-sorted-array/
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start=0
+        end=len(nums)-1
+        while start<=end:
+            mid = (start+end)//2
+            if target==nums[mid]:
+                return mid
+            if nums[start] <= nums[mid]:
+                if nums[start] <= target <= nums[mid]:
+                    end = mid-1
+                else:
+                    start = mid+1
+            else:
+                if nums[mid]<= target <= nums[end]:
+                    start = mid+1
+                else:
+                    end= mid-1
+                
+        return -1
