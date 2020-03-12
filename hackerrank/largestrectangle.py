6bfd7f4e907cf0e71622be288cc144295743caa4
Myungho Sim <danielsim22c@gmail.com>
2020-03-12 01:23:46 -0400

    Update largestrectangle.py

--- hackerrank/largestrectangle.py
+++ hackerrank/largestrectangle.py
@@ -9,12 +9,13 @@ import sys
 # Complete the largestRectangle function below.
 def largestRectangle(h):
     h.append(0)
-    size = len(h)
     i=0
+    area=0
     stack = []
-    while i<size:
+    while i<len(h):
         if not stack or h[stack[-1]]<=h[i]:
             stack.append(i)
+            i+=1
         else:
             top = stack.pop() 
             area = max(area, h[top]*(i-stack[-1]-1 if stack else i))
