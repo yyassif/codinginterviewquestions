e0ce1eb8d564d3447aa85960125693a07c1259bc
Myungho Sim <danielsim22c@gmail.com>
2020-03-16 00:17:39 -0400

    Update 394.decodestring.py

--- algorithms/394.decodestring.py
+++ algorithms/394.decodestring.py
@@ -1,2 +1,29 @@
 #394.decodestring.py
-#sol from https://leetcode.com/problems/decode-string/discuss/508115/Simple-python-with-stack-easy-to-understand
+#sol adopted from https://leetcode.com/problems/decode-string/discuss/508115/Simple-python-with-stack-easy-to-understand
+class Solution:
+    def decodeString(self, s: str) -> str:
+        size=len(s)
+        if size==0:
+            return ""
+        stack=[]
+        cur=""
+        for i in range(size):
+            cur = s[i]
+            if cur.isdigit() or cur.isalpha() or cur=="[":
+                stack.append(cur)
+            else:
+                #get pattern 
+                pat=""
+                while stack and stack[-1]!='[':
+                    pat = stack.pop() +pat
+                stack.pop() #rid of ]
+                
+                #get k val
+                k_str=""
+                while stack and stack[-1].isdigit():
+                   k_str= stack.pop()+k_str
+                k = int(k_str)
+                word = pat*k
+                stack.append(word)
+        return ''.join(stack)
+                    
