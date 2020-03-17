ecffe55c749089fce29d5660cb3265f02e71f8c9
Myungho Sim <danielsim22c@gmail.com>
2020-03-16 23:39:51 -0400

    Update 3.LongestSubstringWithoutRepeatingCharacters.py

--- algorithms/3.LongestSubstringWithoutRepeatingCharacters.py
+++ algorithms/3.LongestSubstringWithoutRepeatingCharacters.py
@@ -17,25 +17,28 @@ class Solution:
             j+=1
         return maxsize
 
-# sol 1 based on leetcode solution in java - sliding window
+#less efficient sliding window sol
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        if n==0 or n==1:
-            return n
-        map={}
+        n= len(s)
+        s=s.replace(" ","#")
+        if n==0:
+            return 0
         i=0
         j=0
-        maxsize=0
-        while i<n and j<n:
-            if map.get(s[j])==None:
-                map[s[j]] = j
+        ans=0
+        map ={}
+        while i<n and j<n:  #sliding window
+            #when s[i] is removed, this section runs again. e.g. abcabcbb
+            if s[j] not in map:  #e.g. abc and then remove a and then this segment runs again with a. a mapped. j=4 and i=1
+                map[s[j]] = 1
                 j+=1
-                maxsize = max(maxsize, j-i)
+                ans = max(ans, j-i)
             else:
-                del map[s[i]]
-                i+=1 #i is updated whenever duplicates are detected.
+                del map[s[i]] #removes s[i] which is left end and s[j]=right end is checked again
+                i+=1
+            print(i,j,ans)
+        return ans
             
-        return maxsize
 
   
