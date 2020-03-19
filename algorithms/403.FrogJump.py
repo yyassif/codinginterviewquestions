14a446d614ec2385a3dc90409b713bc036d90b6d
Myungho Sim <danielsim22c@gmail.com>
2020-03-19 10:29:49 -0400

    Update 403.FrogJump.py

--- algorithms/403.FrogJump.py
+++ algorithms/403.FrogJump.py
@@ -16,3 +16,20 @@ class Solution:
 # Time complexity : O(3^n) Recursion tree can grow upto 3^n
 # Space complexity : O(n) Recursion of depth n is used.
 
+
+# Python BFS DP without recursion
+# https://leetcode.com/problems/frog-jump/discuss/88900/Python-BFS-DP-without-recursion
+
+class Solution(object):
+def canCross(self, stones):
+    steps={key:[] for key in stones}
+    steps[0].append(0)
+    for i in xrange(len(stones)):
+        for j in steps[stones[i]]:
+            for jump in (j-1,j,j+1):
+                nxt = stones[i]+jump
+                if nxt==stones[-1]:
+                    return True
+                if nxt in steps and (not steps[nxt] or steps[nxt][-1]>jump):
+                    steps[nxt].append(jump)
+    return False
