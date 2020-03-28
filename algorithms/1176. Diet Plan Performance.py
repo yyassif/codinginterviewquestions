dfa75264fd30ab7d6fa289780bdce42d37335a9f
Myungho Sim <danielsim22c@gmail.com>
2020-03-27 21:43:58 -0400

    Update 1176. Diet Plan Performance.py

--- algorithms/1176. Diet Plan Performance.py
+++ algorithms/1176. Diet Plan Performance.py
@@ -1,4 +1,22 @@
-# brute force
+#https://leetcode.com/problems/diet-plan-performance/discuss/373642/Python-sliding-window
+def dietPlanPerformance(self, calories, k, lower, upper):
+    points = 0
+    total = sum(calories[0:k])
+    if total < lower:
+        points -= 1
+    if total > upper:
+        points += 1   
+    i = 1
+    while i+k-1 < len(calories):
+        total = total - calories[i-1]+ calories[i+k-1]
+        if total < lower:
+            points -= 1
+        if total > upper:
+            points += 1
+        i += 1
+    return points
+
+# brute force - time limit exceeded
 class Solution:
     def dietPlanPerformance(self, calories: List[int], k: int, lower: int, upper: int) -> int:
         n = len(calories)
