c3d6f1f91e077c2c41a61d284d3c4e6d64b431dd
Myungho Sim <danielsim22c@gmail.com>
2020-04-02 17:08:41 -0400

    Update Substrings of size K with K distinct chars.py

--- amazon_oa/Substrings of size K with K distinct chars.py
+++ amazon_oa/Substrings of size K with K distinct chars.py
@@ -1,4 +1,26 @@
 #Amazon OA question : https://leetcode.com/discuss/interview-question/370112
+#more efficient sol
+s = "abcabc"
+k = 3
+n = len(s)
+def substringk(s, k):
+    if not s or k == 0:
+        return []
+    
+    letter, res = {}, set()
+    start = 0
+    for i in range(len(s)):
+        if s[i] in letter and letter[s[i]] >= start:
+            start = letter[s[i]]+1
+        letter[s[i]] = i
+        if i-start+1 == k:
+            res.add(s[start:i+1])
+            start += 1
+    return list(res)
+ret = substringk(s,k)
+print(ret)
+
+
 #brute force
 s = "abcabc"
 k = 3
