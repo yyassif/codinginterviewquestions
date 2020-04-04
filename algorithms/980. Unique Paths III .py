8314b3e15706850bf585f7d2786e2ee949b776be
Myungho Sim <danielsim22c@gmail.com>
2020-04-03 22:32:56 -0400

    Update 980. Unique Paths III .py

--- algorithms/980. Unique Paths III .py
+++ algorithms/980. Unique Paths III .py
@@ -1,4 +1,42 @@
 #980. Unique Paths III 
+#dynamic programming sol, based on backtracking sol
+#https://leetcode.com/articles/unique-paths-iii/
+from functools import lru_cache
+class Solution:
+    def uniquePathsIII(self, grid):
+        R, C = len(grid), len(grid[0])
+
+        def code(r, c):
+            return 1 << (r * C + c)
+
+        def neighbors(r, c):
+            for nr, nc in ((r-1, c), (r, c-1), (r+1, c), (r, c+1)):
+                if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] % 2 == 0:
+                    yield nr, nc
+
+        target = 0
+        for r, row in enumerate(grid):
+            for c, val in enumerate(row):
+                if val % 2 == 0:
+                    target |= code(r, c)
+
+                if val == 1:
+                    sr, sc = r, c
+                if val == 2:
+                    tr, tc = r, c
+
+        @lru_cache(None)
+        def dp(r, c, todo):
+            if r == tr and c == tc:
+                return +(todo == 0)
+
+            ans = 0
+            for nr, nc in neighbors(r, c):
+                if todo & code(nr, nc):
+                    ans += dp(nr, nc, todo ^ code(nr, nc))
+            return ans
+
+        return dp(sr, sc, target)
 #back tracking sol
 class Solution:
     def uniquePathsIII(self, grid):
