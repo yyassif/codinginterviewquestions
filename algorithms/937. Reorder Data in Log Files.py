74702dce2751d679a302e6bf3d3a7fe6e84a4a89
Myungho Sim <danielsim22c@gmail.com>
2020-04-05 18:08:45 -0400

    Update 937. Reorder Data in Log Files.py

--- algorithms/937. Reorder Data in Log Files.py
+++ algorithms/937. Reorder Data in Log Files.py
@@ -1,10 +1,12 @@
-def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        def getKeys(x):
-            k = x.split(" ")
-            l = k[1].isdigit()
-            if l:
-                return l, []
-            else:
-                return l, k[1:], k[0]
-        
-        return sorted(logs, key=getKeys)
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        def order(x):
+            arr = x.split(" ")
+            #sort :nums first. characters come next
+            is_alpha = arr[1].isnumeric() #check if numbers after ID
+            if is_alpha:
+                return is_alpha, []
+            else: #chars come next
+                #return False, rest of list after id and id
+                return is_alpha, arr[1:], arr[0] 
+        return sorted(logs, key=order)
