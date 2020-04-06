a8be1a25ec97c8f21feb24ad65a1a671a6008b44
Myungho Sim <danielsim22c@gmail.com>
2020-04-06 11:56:17 -0400

    Update 1192. Critical Connections in a Network.py

--- algorithms/1192. Critical Connections in a Network.py
+++ algorithms/1192. Critical Connections in a Network.py
@@ -1,6 +1,7 @@
 # video explanation https://www.youtube.com/watch?v=CsGP_s_3GWg
 # https://www.youtube.com/watch?v=CsGP_s_3GWg
 # algo explnation : https://www.geeksforgeeks.org/articulation-points-or-cut-vertices-in-a-graph/
+# tutorial https://www.youtube.com/watch?v=aZXi1unBdJA&list=PLQealZQ7ajwjIQUoHHnz17KNG-tixkw6P&index=3
 # https://www.youtube.com/watch?v=2kREIkF9UAs
 class Solution:
     import collections
@@ -11,20 +12,20 @@ class Solution:
             dic[v].add(u)
         steps = [-1]*n
         res=[]
-        self.helper(0,-1,0,steps,dic,res)
+        self.dfs(0,-1,0,steps,dic,res)
         return res
         
-    def helper(self, cur, par, level, steps, dic,res): #par=parent
+    def dfs(self, cur, par, level, steps, dic,res): #par=parent
         steps[cur] = level
         
-        for child in dic[cur]:
-            if child == par:
+        for child in dic[cur]: #dic contains all the vertices #child== "to", cur="at"
+            if child == par: #skip parent
                 continue
             if steps[child] == -1: #unvisited
-                min_step = self.helper(child, cur, level+1, steps, dic, res)
-                steps[cur] = min( steps[cur], min_step)
+                min_step = self.dfs(child, cur, level+1, steps, dic, res)
+                steps[cur] = min( steps[cur], min_step) #propagate minimum edge/step down to its children
             else:
                 steps[cur] = min(steps[child],steps[cur])
-        if steps[cur] == level and cur !=0:
+        if steps[cur] == level and cur !=0: #articulation point. not root. its step equals level
             res.append([cur,par])
         return steps[cur]
