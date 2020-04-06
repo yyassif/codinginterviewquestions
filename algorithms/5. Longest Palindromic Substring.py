08c81805c600448cd3d7c35dcaf25aaf88d0731c
Myungho Sim <danielsim22c@gmail.com>
2020-04-05 21:14:07 -0400

    Update 5. Longest Palindromic Substring.py

--- algorithms/5. Longest Palindromic Substring.py
+++ algorithms/5. Longest Palindromic Substring.py
@@ -1,4 +1,30 @@
 #5. Longest Palindromic Substring
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if s is None and n==0:
+            return None
+        start=0
+        end=0
+        def expand(a,b):
+            while 0<=a and b<n and s[a]==s[b]:
+                a-=1
+                b+=1
+            return b-a-1
+        for i in range(n):
+            len1 = expand(i,i)
+            len2 = expand(i,i+1)
+            maxi = max(len1, len2) #find max center length
+            if maxi>end-start: #update max center start, end
+                start = i-(maxi-1)//2
+                end = i+maxi//2
+            print(len1, len2, maxi, start, end)
+        return s[start:end+1]
+        
+            
+                
+                
+                
 
 #brute force time limit exceeded
 class Solution:
