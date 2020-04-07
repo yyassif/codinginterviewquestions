34cd21ca5a235eda4a3814bdcd12f79c1a7b8e65
Myungho Sim <danielsim22c@gmail.com>
2020-04-07 04:40:12 -0400

    Update 59. Spiral Matrix II.py

--- algorithms/59. Spiral Matrix II.py
+++ algorithms/59. Spiral Matrix II.py
@@ -17,6 +17,39 @@
 # X
 # finish with  T ->R->B->L for one each side for the inner 2x2
 
+#my own sol
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        m=[[0 for _ in range(n)] for _ in range(n)]
+        side=n-1
+        r=0
+        c=n-1
+        cnt=1
+        loop=0
+        while (side>=1 and n%2==0) or (side>1 and n%2==1):
+            for i in range(loop,side):
+                m[r][i] = cnt
+                cnt+=1
+            for i in range(loop,side):
+                m[i][c] = cnt
+                cnt+=1
+            for i in range(loop,side):
+                m[n-r-1][n-i-1] = cnt
+                cnt+=1
+            for i in range(loop,side):
+                m[n-i-1][n-c-1]=cnt
+                cnt+=1
+            side-=1
+            r+=1
+            c-=1
+            loop+=1
+        if n%2==1:
+            m[n//2][n//2]=cnt
+        return m
+    
+
+#similar sol from discussions
+
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         cnt=1
