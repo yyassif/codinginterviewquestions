32377c01885c27504e4bf8ec7638c617fddfff31
Myungho Sim <danielsim22c@gmail.com>
2020-04-08 06:30:38 -0400

    Update topkfrequentkeywords.py

--- amazon_oa/topkfrequentkeywords.py
+++ amazon_oa/topkfrequentkeywords.py
@@ -1,4 +1,53 @@
 # https://leetcode.com/discuss/interview-question/542597/
+k = 2
+keywords = ["anacell", "betacellular", "cetracular", "deltacellular", "eurocell"]
+reviews = [
+  "I love anacell Best services; Best services provided by anacell",
+  "betacellular has great services",
+  "deltacellular provides much better services than betacellular",
+  "cetracular is worse than anacell",
+  "Betacellular is better than deltacellular.",
+]
+
+import re
+from collections import Counter
+
+class Element:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+        
+    def __lt__(self, other):
+        if self.freq == other.freq:
+            return self.word > other.word
+        return self.freq < other.freq
+
+def topKFrequent(k, keywords, reviews):
+    '''
+    k: int
+    keywwords: list of string
+    reviews: list of string
+    '''
+    word_list = []
+    
+    for review in reviews:
+        word_list += set(review.lower().replace('[^a-z0-9]', '').split())
+    
+    count = Counter(word_list)
+    
+    heap = []
+    
+    for word, freq in count.items():
+        if word in keywords:
+            heapq.heappush(heap, Element(word, freq))
+            if len(heap) > k:
+                heapq.heappop(heap)
+    
+    return [heapq.heappop(heap).word for _ in range(k)][::-1]
+    
+print(topKFrequent(k, keywords, reviews))
+
+#####################################################################################################
 from re import sub
 from collections import defaultdict
 def top_K_freq_keywords(keywords, reviews, k):
