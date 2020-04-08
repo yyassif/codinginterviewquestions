6f74d75e6494b0d3fb96a9f836238c68a30db781
Myungho Sim <danielsim22c@gmail.com>
2020-04-08 05:46:57 -0400

    Update topkfrequentkeywords.py

--- amazon_oa/topkfrequentkeywords.py
+++ amazon_oa/topkfrequentkeywords.py
@@ -35,3 +35,44 @@ Related problems:
 
 https://leetcode.com/problems/top-k-frequent-words/
 https://leetcode.com/problems/top-k-frequent-elements/
+
+# sol 1
+# space Complexity: O(n)
+# RTC: O(n+k*logn)
+def top(k, keywords, reviews):
+    map = {}
+    for key in keywords:
+        for review in reviews:
+            if key in review.lower():
+                if key in map:
+                    map[key]+=1
+                else:
+                    map[key] = 1
+    ret = list(map)
+    ret.sort(key = lambda x:(-map[x], x))
+    return ret[:k]
+
+k = 2
+keywords = ["anacell", "cetracular", "betacellular"]
+reviews = [
+    "Anacell provides the best services in the city",
+    "betacellular has awesome services",
+    "Best services provided by anacell, everyone should use anacell",
+]
+ret = top(k, keywords, reviews)
+print(ret)
+
+
+k = 2
+keywords = ["anacell", "betacellular", "cetracular", "deltacellular", "eurocell"]
+reviews = [
+  "I love anacell Best services; Best services provided by anacell",
+  "betacellular has great services",
+  "deltacellular provides much better services than betacellular",
+  "cetracular is worse than anacell",
+  "Betacellular is better than deltacellular.",
+]
+ret = top(k, keywords, reviews)
+print(ret)
+
+  
