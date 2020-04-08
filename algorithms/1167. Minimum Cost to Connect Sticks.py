209f923d78c4b9a08e676dd5cdda04c68b792d73
Myungho Sim <danielsim22c@gmail.com>
2020-04-07 22:59:58 -0400

    Update 1167. Minimum Cost to Connect Sticks.py

--- algorithms/1167. Minimum Cost to Connect Sticks.py
+++ algorithms/1167. Minimum Cost to Connect Sticks.py
@@ -1,4 +1,5 @@
 # 1167. Minimum Cost to Connect Sticks
+# e.g. list= [2,4,3] -> [ 2,3,4]   cost_1= 2,3 is 5, 5+4=9,  total cost: 5+9=14
 import heapq
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
