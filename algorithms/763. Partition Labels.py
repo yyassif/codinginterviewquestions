035b9350705253dbc6313016ec0e79bb4ddc2e58
Myungho Sim <danielsim22c@gmail.com>
2020-04-07 20:20:46 -0400

    Create 763. Partition Labels.py

---
+++ algorithms/763. Partition Labels.py
@@ -0,0 +1,12 @@
+# 763. Partition Labels
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        m = {c:i for i,c in enumerate(S)}
+        ret = []
+        j,anchor=0,0
+        for i,c in enumerate(S):
+            j=max(j, m[c]) #check max idx of the label
+            if i==j: #at the end of the current partition
+                ret.append(i-anchor+1)
+                anchor=i+1 #move anchor to the start of the next partition
+        return ret
