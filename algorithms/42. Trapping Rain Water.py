651bb6c5eb0e1d37490b576426329284672390f0
Myungho Sim <danielsim22c@gmail.com>
2020-04-10 23:51:27 -0400

    Update 42. Trapping Rain Water.py

--- algorithms/42. Trapping Rain Water.py
+++ algorithms/42. Trapping Rain Water.py
@@ -1,3 +1,28 @@
+#dynamic programming based on brute force sol
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        if n==0:
+            return 0
+        left_max=[0]*n
+        left_max[0] = height[0]
+        right_max=[0]*n
+        right_max[n-1] = height[n-1]
+        for i in range(1,n):
+            left_max[i]=max(left_max[i-1], height[i])
+            
+        for i in reversed(range(n-1)):
+            right_max[i]=max(right_max[i+1], height[i])
+        print("l", left_max)
+        print("r", right_max)
+        ret=0
+        for i in range(1,n-1):
+            ret+= min(left_max[i], right_max[i])-height[i]
+            print(min(left_max[i], right_max[i]))
+        return ret
+        
+###################################################################
+
 #brute force - time exceeded
 class Solution:
     def trap(self, height: List[int]) -> int:
