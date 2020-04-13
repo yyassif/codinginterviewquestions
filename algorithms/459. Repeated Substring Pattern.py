7c6def06fe842eb7efea00ad93ac7f694c413a5f
Myungho Sim <danielsim22c@gmail.com>
2020-04-13 09:31:06 -0400

    Update 459. Repeated Substring Pattern.py

--- algorithms/459. Repeated Substring Pattern.py
+++ algorithms/459. Repeated Substring Pattern.py
@@ -1,3 +1,17 @@
+#sol 1
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n = len(s)
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                j = n // i
+                if ''.join([s[:i]] * j) == s:
+                    return True
+        return False
+
+#########################################################
+
+#sol 2
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         return s in s[1:] + s[:-1]
