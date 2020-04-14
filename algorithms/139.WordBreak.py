850de69d3c5fe8abfa579512209a364602a101ba
Myungho Sim <danielsim22c@gmail.com>
2020-04-14 05:40:03 -0400

    Update 139.WordBreak.py

--- algorithms/139.WordBreak.py
+++ algorithms/139.WordBreak.py
@@ -1,4 +1,19 @@
 # 139. Word Break from leetcode
+#memoization, recursion - passes all tests
+class Solution(object):
+    def __init__(self):
+        self.d = {}     # without proper init of global vars some tests are failing - a known leetcode issue
+    def wordBreak(self, s, wordDict): 
+        if not s: return False
+        if s in wordDict: return True
+        for w in wordDict:
+            if w==s[:len(w)]:
+                if s[len(w):] not in self.d:
+                    self.d[s[len(w):]] = self.wordBreak(s[len(w):], wordDict)
+                    if  self.d[s[len(w):]]:   return True
+        return False	
+
+#########################################################################
 #brute force solution - passes some cases,time limit exceeded
 s = "cars"
 wordDict = ["car","ca","rs"]
