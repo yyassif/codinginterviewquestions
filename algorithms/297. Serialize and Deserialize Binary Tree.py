9ac888f9d393fb7ce8013900951eff8f71dd336e
Myungho Sim <danielsim22c@gmail.com>
2020-04-13 21:43:18 -0400

    Update 297. Serialize and Deserialize Binary Tree.py

--- algorithms/297. Serialize and Deserialize Binary Tree.py
+++ algorithms/297. Serialize and Deserialize Binary Tree.py
@@ -3,51 +3,59 @@
 # e.g. [1,2,3,null,null,4,5]
 # e.g. encode tree to 1,2,None,None,3,4,None,None,5,None,None
 # Deserialize process run time O(T), space O(1) (O(depth) including calling stack)
-def __init__(self):
-    self.data = []
-    # self.inorder = []
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-def serialize(self, root):
-    """Encodes a tree to a single string.
+class Codec:
 
-    :type root: TreeNode
-    :rtype: str
-    """
-    if not root:
-        return
-    stack = [root]
-    while stack:
-        cur = stack.pop()
-        if cur == None:
-            self.data.append('#')
-            continue
-        self.data.append(cur.val)
-        stack.append(cur.right)
-        stack.append(cur.left)
-    print(self.data)
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        arr = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node==None:
+                arr.append("None")
+                continue
+            else:
+                arr.append(str(node.val))
+            stack.append(node.right)
+            stack.append(node.left)
+        return ','.join(arr)
 
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        arr = data.split(",")
+        # print(arr)
+        def build():
+            if arr is None:
+                return
+            val = arr.pop(0)
+            if val=="None":
+                return
+            else:
+                node = TreeNode(val)
+            node.left = build()
+            node.right = build()
+            return node
+        return build()
+        
 
-
-def deserialize(self, data):
-    """Decodes your encoded data to tree.
-
-    :type data: str
-    :rtype: TreeNode
-    """
-    def build():
-        if not self.data:
-            return
-        data = self.data.pop(0)
-        if data == '#':
-            return
-        else:
-            cur = TreeNode(data)
-        cur.left = build()
-        cur.right = build()
-        return cur
-
-    root = build()
-    return root
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
     
     
     
