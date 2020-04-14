99c44acd42aff7a8994fa602288a36426e957fd5
Myungho Sim <danielsim22c@gmail.com>
2020-04-13 20:37:23 -0400

    Update 297. Serialize and Deserialize Binary Tree.py

--- algorithms/297. Serialize and Deserialize Binary Tree.py
+++ algorithms/297. Serialize and Deserialize Binary Tree.py
@@ -1,35 +1,50 @@
-# level order iterative solution
-from collections import deque
-def serialize(self, root):
-    dq = deque([root])
-    ans = []
-    while dq:
-        node = dq.popleft()
-        if node:
-            ans.append(str(node.val))
-            dq.append(node.left)
-            dq.append(node.right)
-        else:
-            ans.append('')
-    while ans and not ans[-1]: #get rid of empty '' strings in ans array
-        ans.pop()
-    return ','.join(ans)
+Serialize process run time O(T), space O(T)
+Deserialize process run time O(T), space O(1) (O(depth) including calling stack)
+    def __init__(self):
+        self.data = []
+        # self.inorder = []
 
-def deserialize(self, data):
-    if not data:
-        return None
-    nodes = map(lambda s: None if not s else TreeNode(int(s)), data.split(','))
-    nodes.reverse()
-    root = nodes.pop()
-    dq = deque([root])
-    while nodes:
-        node = dq.popleft()
-        node.left = nodes.pop()
-        if node.left:
-            dq.append(node.left)
-        if not nodes:
-            break
-        node.right = nodes.pop()
-        if node.right:
-            dq.append(node.right)
-    return root
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            if cur == None:
+                self.data.append('#')
+                continue
+            self.data.append(cur.val)
+            stack.append(cur.right)
+            stack.append(cur.left)
+        print(self.data)
+        
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        def build():
+            if not self.data:
+                return
+            data = self.data.pop(0)
+            if data == '#':
+                return
+            else:
+                cur = TreeNode(data)
+            cur.left = build()
+            cur.right = build()
+            return cur
+        
+        root = build()
+        return root
+    
+    
+    
