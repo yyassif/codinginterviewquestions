c8dd5619c8aa7ab05fe197b02e428eb4de489ac9
Myungho Sim <danielsim22c@gmail.com>
2020-04-13 20:37:43 -0400

    Update 297. Serialize and Deserialize Binary Tree.py

--- algorithms/297. Serialize and Deserialize Binary Tree.py
+++ algorithms/297. Serialize and Deserialize Binary Tree.py
@@ -1,50 +1,50 @@
-Serialize process run time O(T), space O(T)
-Deserialize process run time O(T), space O(1) (O(depth) including calling stack)
-    def __init__(self):
-        self.data = []
-        # self.inorder = []
+# Serialize process run time O(T), space O(T)
+# Deserialize process run time O(T), space O(1) (O(depth) including calling stack)
+def __init__(self):
+    self.data = []
+    # self.inorder = []
 
-    def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
-        if not root:
+def serialize(self, root):
+    """Encodes a tree to a single string.
+
+    :type root: TreeNode
+    :rtype: str
+    """
+    if not root:
+        return
+    stack = [root]
+    while stack:
+        cur = stack.pop()
+        if cur == None:
+            self.data.append('#')
+            continue
+        self.data.append(cur.val)
+        stack.append(cur.right)
+        stack.append(cur.left)
+    print(self.data)
+
+
+
+def deserialize(self, data):
+    """Decodes your encoded data to tree.
+
+    :type data: str
+    :rtype: TreeNode
+    """
+    def build():
+        if not self.data:
             return
-        stack = [root]
-        while stack:
-            cur = stack.pop()
-            if cur == None:
-                self.data.append('#')
-                continue
-            self.data.append(cur.val)
-            stack.append(cur.right)
-            stack.append(cur.left)
-        print(self.data)
-        
-        
+        data = self.data.pop(0)
+        if data == '#':
+            return
+        else:
+            cur = TreeNode(data)
+        cur.left = build()
+        cur.right = build()
+        return cur
 
-    def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        
-        :type data: str
-        :rtype: TreeNode
-        """
-        def build():
-            if not self.data:
-                return
-            data = self.data.pop(0)
-            if data == '#':
-                return
-            else:
-                cur = TreeNode(data)
-            cur.left = build()
-            cur.right = build()
-            return cur
-        
-        root = build()
-        return root
+    root = build()
+    return root
     
     
     
