ccadda2b5756b7bfc48e29873d0a3656b3887112
Myungho Sim <danielsim22c@gmail.com>
2020-04-16 11:26:19 -0400

    Update 283. Move Zeroes.py

--- algorithms/283. Move Zeroes.py
+++ algorithms/283. Move Zeroes.py
@@ -1,4 +1,17 @@
-#run O(n), space=O(1) sol
+# most efficient run O(n), space=O(1) sol
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        j=0
+        for i in range(n):
+            if nums[i]!=0:
+                nums[i],nums[j] = nums[j],nums[i]
+                j+=1
+####################################################
+#suboptimal run O(n), space=O(1) sol
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
