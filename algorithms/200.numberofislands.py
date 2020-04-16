5eedaa057f5a48bac3f917f9e2245bcaed95c158
Myungho Sim <danielsim22c@gmail.com>
2020-04-16 13:55:44 -0400

    Update 200.numberofislands.py

--- algorithms/200.numberofislands.py
+++ algorithms/200.numberofislands.py
@@ -26,43 +26,42 @@ class Solution:
                     dfs(grid, i,j)
         return cnt
                 
-#version 2 - my own bfs solution. runtime is better than 44%, memory usage is better than 91%
+#version 2 - my own bfs solution. 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        nr = len(grid)
+        nr=len(grid)
         if nr==0:
             return 0
         nc = len(grid[0])
-        que =[]
-        def checkValid(r,c):
-            if 0<=r<nr and 0<=c<nc and grid[r][c]=='1':
-                return True
-            return False
-        def bfs(grid,r,c):
-            if r<0 or c<0 or r>=nr or c>=nc or grid[r][c]=='0':
-                return
-            que.append((r,c))
-            while que:
-                (r,c) = que.pop(0)
-                if checkValid(r-1,c):
-                    grid[r-1][c] = '0'
-                    que.append((r-1,c))
-                if checkValid(r+1,c):
-                    grid[r+1][c] = '0'
-                    que.append((r+1,c))
-                if checkValid(r,c-1):
-                    grid[r][c-1] = '0'
-                    que.append((r,c-1))
-                if checkValid(r,c+1):
-                    grid[r][c+1] = '0'
-                    que.append((r,c+1))
-            
-        cnt =0
+        if nc==0:
+            return 0
+        q= []
+        lands = []
+        #find land
         for i in range(nr):
             for j in range(nc):
-                if grid[i][j] == '1':
-                    cnt +=1
-                    bfs(grid,i,j)
-        return cnt
-                
+                grid[i][j]=int(grid[i][j])
+                if grid[i][j]==1:
+                    lands.append((i,j))
+        dirs= {(0,-1), (0,1), (1,0),(-1,0)}
+        def valid(a,b):
+            if 0<=a<nr and 0<=b<nc and grid[a][b]==1:
+                return True
+            return False
+        
+        def bfs(a,b):
+            q.append((a,b))
+            while q:
+                x,y = q.pop(0)
+                for direc in dirs:
+                    if valid(x+direc[0], y+direc[1]):
+                        q.append((x+direc[0], y+direc[1]))
+                        grid[x+direc[0]][y+direc[1]]=2 #mark as visited
+        ret=0
+        for land in lands:
+            a,b = land
+            if grid[a][b]==1:
+                bfs(a,b)
+                ret+=1
+        return ret
         
