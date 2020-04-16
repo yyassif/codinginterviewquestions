1306c4153819ad6c13123084f308c121e7d7bc9c
Myungho Sim <danielsim22c@gmail.com>
2020-04-16 16:54:16 -0400

    Update 937. Reorder Data in Log Files.py

--- algorithms/937. Reorder Data in Log Files.py
+++ algorithms/937. Reorder Data in Log Files.py
@@ -12,3 +12,12 @@ class Solution:
                 #return False, rest of list after id and id
                 return is_alpha, arr[1:], arr[0] 
         return sorted(logs, key=order)
+
+#####################################################################
+#sol2 comentary sol- run- O(n log n) , space O(n) , n=total content of logs
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        def f(log):
+            id, rest = log.split(" ",1)
+            return (0, rest, id) if rest[0].isalpha() else (1,) # (1,) because all digits are sorted in their original order
+        return sorted(logs, key=f)
