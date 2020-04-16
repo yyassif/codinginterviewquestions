fbd566cfe5649225636c751c8054a78c1620b747
Myungho Sim <danielsim22c@gmail.com>
2020-04-16 15:41:17 -0400

    Update 378.kthsmallestelementinasortedmatrix.py

--- algorithms/378.kthsmallestelementinasortedmatrix.py
+++ algorithms/378.kthsmallestelementinasortedmatrix.py
@@ -12,3 +12,23 @@ class Solution:
             if row+1<n:
                 heapq.heappush(heap,(matrix[row+1][col], row+1, col))  # insert new value from row below. because current row is already in
         return val
+
+#brute force using heap would be O(log(n^2) since you put all the itmes in and use heap to get k items. 
+import heapq
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n=len(matrix)
+        if n==0:
+            return -1
+        arr=[]
+        heapq.heapify(arr)
+        for i in range(n):
+            for j in range(n):
+                if len(arr)<k:
+                    heapq.heappush(arr, -matrix[i][j])
+                else:
+                    if matrix[i][j]<=-arr[0]: #front of the heap is max since multiplied by -1
+                        heapq.heappop(arr)
+                        heapq.heappush(arr, -matrix[i][j])
+                # print(arr,matrix[i][j],arr[-1])
+        return -heapq.heappop(arr)
