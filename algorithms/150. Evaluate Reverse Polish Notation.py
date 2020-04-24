0d17c0d5e8eb6ed1b6ac289733cd78496c000df2
Myungho Sim <danielsim22c@gmail.com>
2020-04-24 12:18:05 -0400

    Create 150. Evaluate Reverse Polish Notation.py

---
+++ algorithms/150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,26 @@
+def evalRPN(self, tokens):
+    
+    stack = []
+    
+    for token in tokens:
+        
+        if token not in "+-/*":
+            stack.append(int(token))
+            continue
+    
+        number_2 = stack.pop()
+        number_1 = stack.pop()
+        
+        result = 0
+        if token == "+":
+            result = number_1 + number_2
+        elif token == "-":
+            result = number_1 - number_2
+        elif token == "*":
+            result = number_1 * number_2
+        else:  # DO NOT USE num1//num2. USE int(num1/num2) like below!!!!!!!!!!!!!!!!!!!!!
+            result = int(number_1 / number_2)   #VERY IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!
+            
+        stack.append(result)
+
+    return stack.pop()
