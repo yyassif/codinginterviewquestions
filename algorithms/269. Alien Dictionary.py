f7afe7f89b6f21c40ca76ff40cf02336155784cd
Myungho Sim <danielsim22c@gmail.com>
2020-04-24 22:49:28 -0400

    Update 269. Alien Dictionary.py

--- algorithms/269. Alien Dictionary.py
+++ algorithms/269. Alien Dictionary.py
@@ -1,29 +1,33 @@
-# https://leetcode.com/problems/alien-dictionary/discuss/581952/94-faster-Kahn's-Algo
-# Kahn's algo and BFS
-chars = {c for x in words for c in x }
+#https://leetcode.com/problems/alien-dictionary/discuss/390614/Python-BFS-Topological-Sorting
+#BFS topological sorting / Kahn's algo
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        # calculate all edges (u->v, in which u must be ahead of v in alien dictionary)
+        edges = defaultdict(set)
+        degrees = defaultdict(int)
+        for two_words in zip(words, words[1:]): # compare adjcent words
+            for ch1, ch2 in zip(*two_words): # compare letter by letter between two words
+                if ch1 != ch2: # ch1 -> ch2 (degree[ch2]++)
+                    edges[ch1].add(ch2) # ch2 depends on (is after) ch1
+                    break 
+        # calculate in-degrees for all vertices
+        for ch in edges.keys():
+            for ch2 in edges[ch]:
+                degrees[ch2] += 1
         
-graph = defaultdict(list)
-i_degree = defaultdict(int)
-
-for word1, word2 in zip(words, words[1:]):
-	for char1, char2 in zip(word1, word2):
-		if char1 != char2:
-			graph[char1].append(char2)
-			i_degree[char2] += 1
-			break
-	else:
-		if len(word1) > len(word2): return ""
-
-Q = [x for x in chars if i_degree[x] == 0]
-L = []
-
-while Q:
-	n = Q.pop()
-	L.append(n)
-
-	for node in graph[n]:
-		i_degree[node] -= 1
-		if i_degree[node] == 0:
-			Q.append(node)
-
-return "".join(L) if len(L) == len(chars) else ""
+        charset = set(''.join(words)) # get all vertices
+        q = [ch for ch in charset if ch not in degrees] # degree=0 as start nodes
+        res = []
+        while q:
+            ch = q.pop(0)
+            res.append(ch)
+            for ch2 in edges[ch]:
+                degrees[ch2] -= 1
+                if degrees[ch2] == 0:
+                    q.append(ch2)
+        if all(map(lambda d: d==0, degrees.values())): # all vertex in degrees are zero, acyclic graphs
+            return ''.join(res)
+        return '' # otherwise, circle found in graph
