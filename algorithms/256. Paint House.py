de626934f1193d118cdfa4a1f1b88d6c99efcd81
Myungho Sim <danielsim22c@gmail.com>
2020-04-26 17:50:14 -0400

    Update 256. Paint House.py

--- algorithms/256. Paint House.py
+++ algorithms/256. Paint House.py
@@ -1,5 +1,31 @@
 #input : nth row represents costs for col 0=red, col 1=blue, col 2 =green for the nth row house.
+#memoization: runtime- O(n), space- O(n)
+from functools import lru_cache
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        self.memo={}
+        if costs == []:
+            return 0
+        @lru_cache(None)
+        def helper(row, color):
+            if (row, color) in self.memo:
+                return self.memo[(row,color)]
+            sumv = costs[row][color]
+            if row==len(costs)-1:
+                return sumv
+            min_val=float('inf')
+            for i in range(len(costs[0])):
+                if i==color:
+                    continue
+                min_val=min(min_val,helper(row+1,i))
+            sumv+=min_val
+            self.memo[(row,color)]=sumv
+            return self.memo[(row,color)]
+        return min(helper(0,0),helper(0,1),helper(0,2))
+    
+##################################################################
 #brute force time limit exceeded 22 / 101 test cases passed.
+#runtime O(2^n) space -O(n)
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
         if costs == []:
