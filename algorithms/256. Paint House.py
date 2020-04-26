44ee3218dbc2b79d08b86b7d99d8dfdce09e9761
Myungho Sim <danielsim22c@gmail.com>
2020-04-26 18:11:05 -0400

    Update 256. Paint House.py

--- algorithms/256. Paint House.py
+++ algorithms/256. Paint House.py
@@ -1,6 +1,18 @@
 #input : nth row represents costs for col 0=red, col 1=blue, col 2 =green for the nth row house.
 #dynamic programming - bottom up. O(n) space and runtime
-#for any number of colors and houses
+def minCost(self, costs: List[List[int]]) -> int:    
+    for n in reversed(range(len(costs) - 1)):
+        # Total cost of painting nth house red.
+        costs[n][0] += min(costs[n + 1][1], costs[n + 1][2])
+        # Total cost of painting nth house green.
+        costs[n][1] += min(costs[n + 1][0], costs[n + 1][2])
+        # Total cost of painting nth house blue.
+        costs[n][2] += min(costs[n + 1][0], costs[n + 1][1])
+
+    if len(costs) == 0: return 0
+    return min(costs[0]) 
+
+#dynamic programming for any number of colors and houses
 #faster if you remove inner for loops and manually set values for 3 colors
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
