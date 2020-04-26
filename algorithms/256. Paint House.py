b231f1f7624c748074c9bf94db026830a12af7fe
Myungho Sim <danielsim22c@gmail.com>
2020-04-26 18:10:37 -0400

    Update 256. Paint House.py

--- algorithms/256. Paint House.py
+++ algorithms/256. Paint House.py
@@ -1,4 +1,23 @@
 #input : nth row represents costs for col 0=red, col 1=blue, col 2 =green for the nth row house.
+#dynamic programming - bottom up. O(n) space and runtime
+#for any number of colors and houses
+#faster if you remove inner for loops and manually set values for 3 colors
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        if costs==[]:
+            return 0
+        for i in reversed(range(len(costs)-1)):
+            for j in range(len(costs[0])):
+                mini=float('inf')
+                for k in range(len(costs[0])):
+                    if j==k:
+                        continue
+                    mini = min(costs[i+1][k],mini)
+                costs[i][j]+=mini
+        print(costs)
+        return min(costs[0])
+            
+        
 #memoization, recursive method: runtime- O(n), space- O(n)
 from functools import lru_cache
 class Solution:
