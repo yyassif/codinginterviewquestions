0667f179847998fc33361b60f190914293757621
Myungho Sim <danielsim22c@gmail.com>
2020-04-27 15:48:15 -0400

    Create 680. Valid Palindrome II.py

---
+++ algorithms/680. Valid Palindrome II.py
@@ -0,0 +1,19 @@
+#brute force time limit exceeded, time- O(n^2), space - O(n)
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def valid(string):
+            n = len(string)
+            for i in range(n//2):
+                if string[i]!=string[n-i-1]:
+                    return False
+            return True
+        if valid(s):
+            return True
+        else:
+            if valid(s[1:]):
+                return True
+            for i in range(1,len(s)):
+                new_s = s[:i]+s[i+1:]
+                if valid(new_s):
+                    return True
+        return False
