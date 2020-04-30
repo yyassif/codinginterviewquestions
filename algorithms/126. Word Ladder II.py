cdf7370b725877e9a0bcc9d2a33aedecfd080bc9
Myungho Sim <danielsim22c@gmail.com>
2020-04-30 18:15:44 -0400

    Create 126. Word Ladder II.py

---
+++ algorithms/126. Word Ladder II.py
@@ -0,0 +1,40 @@
+import collections
+class Solution(object):
+    def findLadders(self, beginWord, endWord, wordList):
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return []
+        search_que = collections.deque()
+        visited = {}
+        word_len = len(beginWord)
+        visited[beginWord] = 1
+        word_dict = collections.defaultdict(set)
+        for word in wordList:
+            for i in range(len(word)):
+                new = word[:i] + "?" + word[i+1:]
+                word_dict[new].add(word)
+        prev_dic = {}
+        break_flag = False
+        search_que.append((beginWord, [], 1))
+        rst = []
+        shortest = None
+        while search_que:
+            word_tup = search_que.popleft()
+            word, path, dist = word_tup
+            if shortest and dist > shortest:
+                break
+            for ind in range(word_len):
+                cur_word = word[:ind] + "?" + word[ind+1:]
+                neighbors = word_dict[cur_word]
+                for neighbor in neighbors:
+                    if neighbor == word:
+                        continue
+                    tup = (neighbor, path + [word], dist + 1)
+                    if neighbor == endWord:
+                        rst.append(tup[1] + [endWord])
+                        shortest = dist
+                        continue
+                    if neighbor not in visited or visited[neighbor] >= dist:
+                        search_que.append(tup)
+                        visited[neighbor] = dist
+        return rst  
