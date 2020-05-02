fa1eb872924319261150c0299a4429628fcf2849
Myungho Sim <danielsim22c@gmail.com>
2020-05-02 01:15:02 -0400

    Update 1236. Web Crawler.py

--- algorithms/1236. Web Crawler.py
+++ algorithms/1236. Web Crawler.py
@@ -1,3 +1,18 @@
 # 1236. Web Crawler
-from urlparse import urlparse
-print urlparse("http://www.website.com/abc/xyz.html").hostname
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        #find host
+        idx = startUrl[7:].find("/")
+        hostname = 'http://' + startUrl[7:idx]
+        
+        stack=[startUrl]
+        result=set([startUrl]) #set is faster
+        
+        while stack:
+            link = stack.pop()
+            urls = htmlParser.getUrls(link)
+            for url in urls:
+                if url.find(hostname)!=-1 and url not in result:
+                    stack.append(url)
+                    result.add(url)
+        return result
