af1a5501ab8e2350c9a29b70124ffcb95ae631a3
Myungho Sim <danielsim22c@gmail.com>
2020-05-03 23:27:13 -0400

    Update 31. Next Permutation.py

--- algorithms/31. Next Permutation.py
+++ algorithms/31. Next Permutation.py
@@ -14,5 +14,19 @@ https://www.youtube.com/watch?v=PYXl_yY-rms
 1257641
 3) reverse all numbers after 5
 1251467
-
-
+#from https://leetcode.com/problems/next-permutation/discuss/14054/Python-solution-with-comments.
+def nextPermutation(self, nums):
+    i = j = len(nums)-1
+    while i > 0 and nums[i-1] >= nums[i]:
+        i -= 1
+    if i == 0:   # nums are in descending order
+        nums.reverse()
+        return 
+    k = i - 1    # find the last "ascending" position
+    while nums[j] <= nums[k]:
+        j -= 1
+    nums[k], nums[j] = nums[j], nums[k]  
+    l, r = k+1, len(nums)-1  # reverse the second part
+    while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l +=1 ; r -= 1
