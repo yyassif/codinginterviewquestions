1a29c36a580891ab33f546a9b61d7b5bf1da6d9e
Myungho Sim <danielsim22c@gmail.com>
2020-05-03 23:11:24 -0400

    Update 31. Next Permutation.py

--- algorithms/31. Next Permutation.py
+++ algorithms/31. Next Permutation.py
@@ -1,2 +1,18 @@
 #video tutorial
 https://www.youtube.com/watch?v=PYXl_yY-rms
+# e.g. 123
+# 123
+# 132  #reverse numbers after 1
+# 213
+# 231
+# 312
+# 321  <- all numbers are in decreasing order
+# [algorithm]
+1) find the first decreasing number when going backwards from the back. 1247651 <--4
+2) swap with the number with the largest decrease. (or a number just larger than the number from step)
+12476*5*1   <--5 in this case. swap with 4
+1257641
+3) reverse all numbers after 5
+1251467
+
+
