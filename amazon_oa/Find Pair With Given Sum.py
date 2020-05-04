b03d2123915c26da59d38bf980f0e82f50a0d123
Myungho Sim <danielsim22c@gmail.com>
2020-05-04 17:06:40 -0400

    Update Find Pair With Given Sum.py

--- amazon_oa/Find Pair With Given Sum.py
+++ amazon_oa/Find Pair With Given Sum.py
@@ -1,5 +1,26 @@
+class Solution():
+    def FindPair(self, nums, target):
+        assert isinstance(nums, list)
+        assert isinstance(target, int)
+
+        if nums is None:
+            return None
+
+        target = target - 30
+        dic = {}
+        ans = []
+        for i in range(len(nums)):
+            if target - nums[i] in dic:
+                ans.append([dic[target - nums[i]], i])
+            dic[nums[i]] = i
+        return max(ans)
+    
+
+#####################################################
+#sol 2
 nums = [20, 50, 40, 25, 30, 10]
 target = 90-30
+
 map ={}
 x=0 #x is bigger number in the pair
 y=0 #y is smaller number in the pair
