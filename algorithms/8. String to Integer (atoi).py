3fdd6febd4ae8574b1a0a87d68dc68e803fe2fea
Myungho Sim <danielsim22c@gmail.com>
2020-05-06 21:37:32 -0400

    Create 8. String to Integer (atoi).py

---
+++ algorithms/8. String to Integer (atoi).py
@@ -0,0 +1,55 @@
+#for loop and many if stmts 
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        num=0
+        is_neg=False
+        is_pos=False
+        non_space=False
+        neg=1
+        map ={"1":1,"2":2,"3":3,"4":4,"5":5, "6":6, "7":7, "8":8,"9":9,"0":0}
+        num_found=False
+        for d in str:
+            print("d",d)
+            # if num_found==False and d.isalpha() and d!="-" and d!="+":
+            #     break
+            if d=="+":
+                if num_found==True:
+                    break
+                if is_pos==True or is_neg==True:
+                    break
+                if is_neg==True or num_found==True:
+                    return 0
+                is_pos=True
+            elif d=="-" :
+                if num_found==True:
+                    break
+                if is_neg==True or is_pos==True:
+                    break
+                if is_pos==True or num_found==True or is_neg==True:
+                    return 0
+                else:
+                    is_neg=True
+            elif d==".":
+                if num_found==True:
+                    break
+                else:
+                    return 0
+            elif d.isdigit():
+                num_found=True
+                neg=-1 if is_neg==True else 1
+                print(neg*num*10+map[d])
+                if (num*10+map[d])>2**31 and neg==-1:
+                    return -2**31
+                elif neg*num*10+map[d]>2**31-1:
+                    return 2**31-1
+                else:
+                    num = num*10+map[d]
+            elif d==' ' and non_space==False:
+                continue
+            elif d==' ' and non_space==True:
+                break
+            else:
+                break
+            if d!=' ':
+                non_space=True
+        return num*neg
