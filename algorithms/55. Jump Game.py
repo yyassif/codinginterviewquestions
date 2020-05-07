a970cf532dcef8fcc68defd2721d76bd145ccc09
Myungho Sim <danielsim22c@gmail.com>
2020-05-06 23:49:02 -0400

    Update 55. Jump Game.py

--- algorithms/55. Jump Game.py
+++ algorithms/55. Jump Game.py
@@ -1,3 +1,3 @@
-approach 1: backtracking run- O(2^n)
+approach 1: backtracking run- O(2^n) , space - O(n)
 Approach 2: Dynamic Programming Top-down run- bottom up/top down O(n^2)
 Approach 2: greedy - runtime O(n)
