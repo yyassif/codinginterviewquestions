ce5083d508990dc31442f0ceb7a07e8bb3c33707
Myungho Sim <danielsim22c@gmail.com>
2020-05-08 19:34:13 -0400

    Update Substrings of size K with K distinct chars.py

--- amazon_oa/Substrings of size K with K distinct chars.py
+++ amazon_oa/Substrings of size K with K distinct chars.py
@@ -29,21 +29,3 @@ def test_driver(s,k, result):
 test_driver("pqpqs", 2, 7)
 test_driver("aabab", 3,0)
 
-###################################################################
-#brute force
-s = "abcabc"
-k = 3
-n = len(s)
-ret = []
-for i in range(n-k+1):
-    map = {}
-    for j in range(k):
-        c = s[i+j]
-        if c in map:
-            break
-        else:
-            map[c] = 1
-    print(s[i:i+k])
-    if len(map)==k and s[i:i+k] not in ret:
-        ret.append(s[i:i+k])
-print(ret)
