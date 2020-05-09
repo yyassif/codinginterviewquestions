1e536fc1c9921c4f37cee5307c62d6e2eb2425ff
Myungho Sim <danielsim22c@gmail.com>
2020-05-09 00:44:26 -0400

    Update 560. Subarray Sum Equals K.py

--- algorithms/560. Subarray Sum Equals K.py
+++ algorithms/560. Subarray Sum Equals K.py
@@ -1,2 +1,31 @@
-#cumulative sum and without space method - time exceeded O(n^2)
+
 # hashmap method - O(n) runtime/space
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+        total=0
+        cnt=0
+        dic={0:1}
+        idx=0
+        for i in range(n):
+            total+=nums[i]
+            if total-k in dic:
+                cnt+= dic[total-k]
+            dic[total] =dic.get(total, 0)+1
+        print(dic)
+        return cnt
+                
+
+#cumulative sum and without space method - time exceeded O(n^2)
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+        total=0
+        cnt=0
+        for i in range(n):
+            total=0
+            for j in range(i,n):
+                total+=nums[j]
+                if total==k:
+                    cnt+=1
+        return cnt
