95745c23fccb1bb075a18b810c0ed262298f09e9
Myungho Sim <danielsim22c@gmail.com>
2020-05-09 20:58:20 -0400

    Update 17. Letter Combinations of a Phone Number.py

--- algorithms/17. Letter Combinations of a Phone Number.py
+++ algorithms/17. Letter Combinations of a Phone Number.py
@@ -1,4 +1,22 @@
 #backtracking space/runtime O(3^N * 4^M) where N is the number of 3 letter numbers and M for 4 letter numbers
+#BFS solution STACK
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits=="":
+            return []
+        dic= {"2":"abc","3":"def","4":"ghi", "5":"jkl","6":"mno", "7":"pqrs","8":"tuv", "9":"wxyz"}
+        output=set()
+        stack = [("",digits)]
+        while stack:
+            combi,arr = stack.pop()
+            if len(arr)==0:
+                output.add(combi)
+                continue
+            for letter in dic[arr[0]]:
+                stack.append((combi+letter, arr[1:]))
+        return output
+                
+#backtracking/BFS recursive sol
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if digits=="":
