9883702b1217abca92ce41ab11384db8d3293ef7
Myungho Sim <danielsim22c@gmail.com>
2020-05-09 20:21:12 -0400

    Update 17. Letter Combinations of a Phone Number.py

--- algorithms/17. Letter Combinations of a Phone Number.py
+++ algorithms/17. Letter Combinations of a Phone Number.py
@@ -6,8 +6,8 @@ class Solution:
         map= {"2":"abc","3":"def","4":"ghi", "5":"jkl","6":"mno", "7":"pqrs","8":"tuv", "9":"wxyz"}
         output=set()
         def backtrack(combi, next_digits):
-            if len(next_digits)==0:
-                output.add(combi)
+            if len(next_digits)==0: #finished permutation. no more letters
+                output.add(combi) #add finished string to output
             else:
                 digit = next_digits[0]
                 letters= map[digit]
