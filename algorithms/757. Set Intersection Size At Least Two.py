e6c925a75a8dfe297a42b5f8ff5cf0e53089338a
Myungho Sim <danielsim22c@gmail.com>
2020-05-10 22:24:35 -0400

    Update 757. Set Intersection Size At Least Two.py

--- algorithms/757. Set Intersection Size At Least Two.py
+++ algorithms/757. Set Intersection Size At Least Two.py
@@ -1,3 +1,17 @@
+#greedy sol https://leetcode.com/problems/set-intersection-size-at-least-two/discuss/439986/Python-keep-track-of-the-right-most-2-points
+ def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x:x[1])
+        res=0
+        cur=[]
+        for start,end in intervals:
+            if not cur or start>cur[1]:
+                res+=2
+                cur=[end-1,end]
+            elif start>cur[0]:
+                res+=1
+                cur=[cur[1],end]
+        return res
+    
 #https://leetcode.com/problems/set-intersection-size-at-least-two/discuss/279406/Python-O(nlogn)-always-peek-the-top-two-elements-of-maxheap
 class Solution(object):
     def intersectionSizeTwo(self, intervals):
