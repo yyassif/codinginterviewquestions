51d940460ad04178b081119ba57acd8388738854
Myungho Sim <danielsim22c@gmail.com>
2020-05-10 21:22:47 -0400

    Update 64. Minimum Path Sum.py

--- algorithms/64. Minimum Path Sum.py
+++ algorithms/64. Minimum Path Sum.py
@@ -1,3 +1,25 @@
+#study both 2d and 1d methods run-O(M*N)  space O(M)
+#1d method
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        if n==0 or m==0:
+            return 0
+        dp = [0 for _ in range(m)]
+        for i in range(n-1,-1,-1):
+            for j in range(m-1, -1,-1):
+                if i==n-1 and j!=m-1:  #last row of grid
+                    dp[j] = grid[i][j]+dp[j+1]
+                elif j==m-1 and i!=n-1: #last col of grid
+                    dp[j] = grid[i][j]+dp[j]
+                elif j!=m-1 and i!=n-1:
+                    dp[j] = grid[i][j]+min(dp[j],dp[j+1])
+                else:
+                    dp[j] = grid[i][j]
+        return dp[0]
+
+#2d method run/space-O(M*N)
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         nr = len(grid)
