8b2bfd6d568cdd8566a27c377b06327595a35731
Myungho Sim <danielsim22c@gmail.com>
2020-05-14 02:34:15 -0400

    Update 981. Time Based Key-Value Store.py

--- algorithms/981. Time Based Key-Value Store.py
+++ algorithms/981. Time Based Key-Value Store.py
@@ -1,36 +1,25 @@
-#44/45 cases passed time limit exceeded
+#time O(1), space O(n)
+import collections
+import bisect
 class TimeMap:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.kv_map = {}
+        self.map = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        print("set ", key,value, timestamp)
-        if key in self.kv_map:
-            self.kv_map[key][timestamp] = value
-        else:
-            self.kv_map[key] = {timestamp:value}
+        self.map[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        print("get ", key, timestamp)
-        prev_time=[]
-        if key in self.kv_map:
-            for time in self.kv_map[key]:
-                if time==timestamp:
-                    return self.kv_map[key][time]
-                elif time>timestamp:
-                    break
-                if len(prev_time)==0 or time>prev_time[-1]:
-                    prev_time.append(time)
-            if len(prev_time)>0:
-                return self.kv_map[key][prev_time[-1]]
-            else:
-                return ""
-            
-        return ""
+        if self.map.get(key, None) is None:
+            return ""
+        arr = self.map[key]
+        #bisect returns idx after the value (point to insert)
+        idx = bisect.bisect_left(arr,(timestamp,chr(127)))
+        t,v = self.map[key][idx-1] #idx-1 b/c it's matched idx or before
+        return v
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
