5d79baaf9a39b8ed929b2ed06ac6ccbad0ec736e
Myungho Sim <danielsim22c@gmail.com>
2020-05-14 00:56:00 -0400

    Update 41.firstmissingpositive.py

--- algorithms/41.firstmissingpositive.py
+++ algorithms/41.firstmissingpositive.py
@@ -1,5 +1,27 @@
 #41.firstmissingpositive.py
 #reference https://leetcode.com/articles/first-missing-positive/
+#cycle sort
+Another O(n) solution use cycle sort:
+
+def firstMissingPositive(self, nums: List[int]) -> int:
+    i = 0
+    n = len(nums)
+    while i < n:
+        j = nums[i] - 1
+        # put num[i] to the correct place if nums[i] in the range [1, n]
+        if 0 <= j < n and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+    # so far, all the integers that could find their own correct place 
+    # have been put to the correct place, next thing is to find out the
+    # place that occupied wrongly
+    for i in range(n):
+        if nums[i] != i + 1:
+            return i + 1
+    return n + 1
+
+#leet code sol
 class Solution:
     def firstMissingPositive(self, nums):
         """
