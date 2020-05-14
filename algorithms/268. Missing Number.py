9b77c06a403d6c1c78a9271eacf093e1ee95f6ec
Myungho Sim <danielsim22c@gmail.com>
2020-05-13 23:44:25 -0400

    Update and rename missingnumber.py to 268. Missing Number.py

---
+++ algorithms/268. Missing Number.py
@@ -0,0 +1,44 @@
+#Myungho Sim
+
+Approach #3 Bit Manipulation [Accepted]  run O(n) / O(1) space
+#e.g. >>> (0^0)^(1^1)^(2^3)^3
+2
+missing number means from there should be numbers 0 to n+1 but with one missing
+class Solution:
+    def missingNumber(self, nums):
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
+
+Approach #4 Gauss' Formula [Accepted] run O(n) / O(1) space
+class Solution:
+    def missingNumber(self, nums):
+        expected_sum = len(nums)*(len(nums)+1)//2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
+
+#sol
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n==0:
+            return 0
+        arr = [i for i in range(n+1)] #since one number is missing
+        for num in arr:
+            if num not in nums:
+                return num
+        
+
+#missing number
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        size = len(nums)
+        arr = [-1] * (size+1)
+        for i in range(size):
+            arr[nums[i]]=1
+        try:
+            num = arr.index(-1)
+        except:
+            num=-1
+        return num
