33fa0612dae2648c1ec3ab6a4f89fd5fd43e5793
Myungho Sim <danielsim22c@gmail.com>
2020-05-15 04:44:41 -0400

    Update 84. Largest Rectangle in Histogram.py

--- algorithms/84. Largest Rectangle in Histogram.py
+++ algorithms/84. Largest Rectangle in Histogram.py
@@ -4,9 +4,10 @@ class Solution:
         stack = []
         heights = [0] + heights + [0]
         for i in range(len(heights)):
+            #add to stack if heights increase and keep popping as smaller heights found
             while(stack and heights[stack[-1]] > heights[i]):
                 h = heights[stack.pop()]
                 w = i-stack[-1]-1
-                ans = max(ans, w*h)
-            stack.append(i)
+                ans = max(ans, w*h)  #keep updating max area
+            stack.append(i) #keep adding index if heights increase
         return ans
