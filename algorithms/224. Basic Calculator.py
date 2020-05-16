8efc513bd3b23921bb589905fc60b047781103ad
Myungho Sim <danielsim22c@gmail.com>
2020-05-16 07:07:58 -0400

    Create 224. Basic Calculator.py

---
+++ algorithms/224. Basic Calculator.py
@@ -0,0 +1,31 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        def eval(stack):
+            res= stack.pop() if stack else 0
+            while stack and stack[-1]!=')':
+                sign =stack.pop()
+                if sign=='+':
+                    res+=stack.pop()
+                else:
+                    res-=stack.pop() 
+            return res
+        stack = []
+        n, operand = 0,0
+        for i in range(len(s)-1, -1,-1):
+            c = s[i]
+            if c.isdigit():
+                operand = (10**n*int(c)) + operand
+                n+=1
+            elif c!=" ":
+                if n:
+                    stack.append(operand)
+                    n, operand =0,0
+                if c=='(':
+                    res =eval(stack)
+                    stack.pop() #pop closing )
+                    stack.append(res)
+                else:
+                    stack.append(c)
+        if n:
+            stack.append(operand)
+        return eval(stack)
