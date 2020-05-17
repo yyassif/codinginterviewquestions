2168177ba257be3ed12e4b112691409834e090f4
Myungho Sim <danielsim22c@gmail.com>
2020-05-17 09:06:18 -0400

    Update 91. Decode Ways.py

--- algorithms/91. Decode Ways.py
+++ algorithms/91. Decode Ways.py
@@ -1,27 +1,19 @@
+#run/space O(1)
 class Solution:
     def numDecodings(self, s: str) -> int:
         if not s:
             return 0
-        memo= {}
-        def recursive_with_memo(index) -> int:
-            # If you reach the end of the string
-            # Return 1 for success.
-            if index == len(s):
+        memo = {}
+        n = len(s)
+        def rec(i):
+            if i==n:
                 return 1
-
-            # If the string starts with a zero, it can't be decoded
-            if s[index] == '0':
+            if s[i]=='0':
                 return 0
-
-            if index == len(s)-1:
+            if i==n-1:
                 return 1
-
-            # Memoization is needed since we might encounter the same sub-string.
-            if index in memo:
-                return memo[index]
-
-            memo[index]= recursive_with_memo(index+1) \
-            + (recursive_with_memo(index+2) if (int(s[index : index+2]) <= 26)
-             else 0)
-            return memo[index]
-        return recursive_with_memo(0)
+            if i in memo:
+                return memo[i]
+            memo[i] = rec(i+1) + (rec(i+2) if int(s[i:i+2])<=26 else 0)
+            return memo[i]
+        return rec(0)
