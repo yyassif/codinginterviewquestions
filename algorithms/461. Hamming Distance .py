9df7bc5c685f5f5f439676f83ca76a20390b122a
Myungho Sim <danielsim22c@gmail.com>
2020-05-17 17:51:37 -0400

    Create 461. Hamming Distance .py

---
+++ algorithms/461. Hamming Distance .py
@@ -0,0 +1,27 @@
+#O(1) Kernighan's method
+class Solution:
+    def hammingDistance(self, x, y):
+        xor = x ^ y
+        distance = 0
+        while xor:
+            distance += 1
+            # remove the rightmost bit of '1'
+            xor = xor & (xor - 1)
+        return distance
+#O(1) method but slower than Kernighan's
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        xor = x ^ y
+        distance = 0
+        while xor:
+            # mask out the rest bits
+            if xor & 1:
+                distance += 1
+            xor = xor >> 1
+        return distance
