4fb3863f0039188fb30412e1afc3e535abb0cc47
Myungho Sim <danielsim22c@gmail.com>
2020-05-17 09:13:40 -0400

    Update 91. Decode Ways.py

--- algorithms/91. Decode Ways.py
+++ algorithms/91. Decode Ways.py
@@ -1,4 +1,20 @@
-#recursive sol run/space O(1)
+#iterative sol run/space O(n)
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s:
+            return 0
+        n = len(s)
+        dp = [0 for _ in range(n+1)]
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+        for i in range(2, n+1):
+            if s[i-1]!='0':
+                dp[i] +=dp[i-1]
+            two_digit = int(s[i-2:i])
+            if two_digit >=10 and two_digit <=26:
+                dp[i] +=dp[i-2]
+        return dp[n]
+#recursive sol run/space O(n)
 # '0' doesn't have a single digit decode.
 class Solution:
     def numDecodings(self, s: str) -> int:
