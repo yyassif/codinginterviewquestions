aad38179741b39d4ada5082bdd0a93d2073385f8
Myungho Sim <danielsim22c@gmail.com>
2020-05-17 10:06:39 -0400

    Create 199. Binary Tree Right Side View.py

---
+++ algorithms/199. Binary Tree Right Side View.py
@@ -0,0 +1,21 @@
+#DFS O(N) for space/run
+class Solution(object):
+    def rightSideView(self, root):
+        rightmost_value_at_depth = dict() # depth -> node.val
+        max_depth = -1
+
+        stack = [(root, 0)]
+        while stack:
+            node, depth = stack.pop()
+
+            if node is not None:
+                # maintain knowledge of the number of levels in the tree.
+                max_depth = max(max_depth, depth)
+
+                # only insert into dict if depth is not already present.
+                rightmost_value_at_depth.setdefault(depth, node.val)
+
+                stack.append((node.left, depth+1))
+                stack.append((node.right, depth+1))
+
+        return [rightmost_value_at_depth[depth] for depth in range(max_depth+1)]
